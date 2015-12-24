163d88c5210ef459b5ada01c10c23908d6149b0d
Elon Rubin <emrubi@umich.edu>
2015-12-24 15:26:59 -0500

    add logout function with DELETE

--- server.js
+++ server.js
@@ -148,19 +148,30 @@ app.post('/users', function(req, res) {
 // POST /users/login
 app.post('/users/login', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
-	
+
+	var userInstance;
+
 	db.user.authenticate(body).then(function(user){
 		var token = user.generateToken('authentication');
-		if(!token){
-			res.status(401).send();
-		}
-		else{
-			res.header('Auth', token).json(user.toPublicJSON());	
-		}
-	}, function(e){
+		userInstance = user;
+		return db.token.create({
+			token: token
+		});
+	}).then(function(tokenInstance){
+		res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+	}).catch(function(){
 		res.status(401).send();
 	});
 });
+
+app.delete('/users/logout', middleware.requireAuthentication, function(req, res){
+	req.token.destroy().then(function(){
+		res.status(204).send();
+	}).catch(function(){
+		res.status(500).send();	
+	});
+});
+
 
 db.sequelize.sync({force: true}).then(function() {
 	app.listen(PORT, function() {
